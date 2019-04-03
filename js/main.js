ec0354f6a0dcb5a1d7ecb8682ad4a64f0a22c4a7
Ahkyhabib <codeclubdetroit@gmail.com>
2019-04-03 19:31:01 -0400

    First commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.show_form').click(function(){
+        $('#form_hide').show(200);
+        $('.hide_card').hide();
+    })
+  });
