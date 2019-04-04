ece4ab7a5cf6675fd73586ce30cca21504534dde
Akhirah Strong <akhirahstrong@quickenloans.com>
2019-04-04 14:53:53 -0400

    Active side nav menu and nav bar menu options

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
 
-
+//The side nav bar for mobile
+//Show contact form  
 $(document).ready(function(){
     $('.sidenav').sidenav();
-    $('.show_form').click(function(){
-        $('#form_hide').show(200);
+    $('.show_form').on('click', function(){
+        $('#form_hide').fadeIn(200);
         $('.hide_card').hide();
+    })
+    //Show about card, and hide all other cards
+    $('.about_card').on('click', function(){
+        event.preventDefault();
+        $('.hide_card_2').fadeIn(200);
+        $('.hide_card, .hide_card_3, #form_hide').hide();
+    })
+    //Show product card, and hide all other cards
+    $('.products_card').on('click', function(){
+        event.preventDefault();
+        $('.hide_card_3').fadeIn(200);
+        $('.hide_card, .hide_card_2, #form_hide').hide();
+    })
+    //Show cantact card, and hide all other cards
+    $('.contact_card').on('click', function(){
+        event.preventDefault();
+        $('#form_hide').fadeIn(200);
+        $('.hide_card, .hide_card_2, .hide_card_3').hide();
     })
   });
