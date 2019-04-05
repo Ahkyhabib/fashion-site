da113d6b6094dfa7216c53f348f5711c938091ad
Akhirah Strong <akhirahstrong@quickenloans.com>
2019-04-05 11:57:42 -0400

    Add thank you card

--- js/main.js
+++ js/main.js
@@ -11,18 +11,29 @@ $(document).ready(function(){
     $('.about_card').on('click', function(){
         event.preventDefault();
         $('.hide_card_2').fadeIn(200);
-        $('.hide_card, .hide_card_3, #form_hide').hide();
+        $('.hide_card, .hide_card_3, .form_hide').hide();
     })
     //Show product card, and hide all other cards
     $('.products_card').on('click', function(){
         event.preventDefault();
         $('.hide_card_3').fadeIn(200);
-        $('.hide_card, .hide_card_2, #form_hide').hide();
+        $('.hide_card, .hide_card_2, .form_hide').hide();
     })
     //Show cantact card, and hide all other cards
     $('.contact_card').on('click', function(){
         event.preventDefault();
         $('#form_hide').fadeIn(200);
         $('.hide_card, .hide_card_2, .hide_card_3').hide();
+    })
+    
+    //Hire contact form, and show thank you form
+    $('.display_form').on('click', function(){
+        $('.slide_down').hide();
+        $('.thankyou_card').fadeIn(200);
+    }) 
+
+    //Hide thank you card
+    $('.about_card, .products_card, .contact_card').on('click', function(){
+        $('.thankyou_card').hide();
     })
   });
